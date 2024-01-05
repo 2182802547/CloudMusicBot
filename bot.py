4745d6219d804d073015c9363b1b459808dffe52
Xiang Li <2182802547@qq.com>
2024-01-05 12:37:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,8 +57,8 @@ class Bot:
 
     def __getUserTask(self) -> [bool, json]:
         taskData = self.session.get(url=self.taskDataUrl).json()["data"]
-        count = taskData.get("count", 0)  # 默认值为0，您可以根据需要设置其他默认值
-        completedCount = taskData.get("completedCount", 0)  # 默认值为0，您可以根据需要设置其他默认值
+        count = taskData.get("count", 5)  # 默认值为0，您可以根据需要设置其他默认值
+        completedCount = taskData.get("completedCount", 5)  # 默认值为0，您可以根据需要设置其他默认值
         todayTask = f"[{completedCount}/{count}]"
         complete = count == completedCount
         self.log.info(f'今日任务：{"已完成" if complete else "未完成"}{todayTask}')
